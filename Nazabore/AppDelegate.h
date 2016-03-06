2608ea0b00b1729339e8f2f63298c634c0e74ac9
e.tyutyuev <e.tyutyuev@2gis.ru>
2016-03-06 12:30:08 +0600

    Initial Commit

---
+++ Nazabore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nazabore
+//
+//  Created by Eugene Tutuev on 06/03/16.
+//  Copyright © 2016 Eugene Tutuev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
