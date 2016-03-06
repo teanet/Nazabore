e1322aa4d2bdbdaa394577c0e139586c12f38fb3
e.tyutyuev <e.tyutyuev@2gis.ru>
2016-03-06 18:21:30 +0600

    update pod

---
+++ NZBLibrary/NZBLibraryDemo/NZBLibraryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NZBLibraryDemo
+//
+//  Created by Eugene Tutuev on 06/03/16.
+//  Copyright © 2016 Eugene Tutuev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
