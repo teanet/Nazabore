ee420556fd87968b532a65dfea3a0f796d2c2cfe
e.tyutyuev <e.tyutyuev@2gis.ru>
2016-04-03 09:54:53 +0600

    [NZB-51] Add localization, fix: #51

---
+++ Nazabore/NZBLocalizableConstants.h
@@ -0,0 +1,17 @@
+#define kNZB_BUTTON_CANCEL_TITLE ZBS(@"button_cancel_title", @"Отмена")
+
+#define kNZB_DATE_FEW_SECONDS_AGO ZBS(@"date_few_seconds_ago", @"Несколько секунд назад")
+#define kNZB_DATE_FEW_MINUTES_AGO ZBS(@"date_few_minutes_ago %ld", @"%ld минуты назад")
+#define kNZB_DATE_SEVERAL_MINUTES_AGO ZBS(@"date_several_minutes_ago %ld", @"%ld минут назад")
+
+#define kNZB_EMOJI_PICKER_TITLE ZBS(@"emoji_picker_title", @"Выберите приколюшку")
+#define kNZB_EMOJI_PICKER_DESCRIPTION ZBS(@"emoji_picker_description", @"Она отобразится на карте\nи около надписи когда-нибудь")
+#define kNZB_EMOJI_PICKER_ANY_EMOJI_BUTTON_TITLE ZBS(@"emoji_picker_any_emoji_button_title", @"Давай любую")
+
+#define kNZB_ERROR_ALERT_TITLE ZBS(@"error_alert_title", @"Ошибка")
+#define kNZB_ERROR_ALERT_BUTTON_OK_TITLE ZBS(@"error_alert_button_ok_title", @"OK")
+
+#define kNZB_INPUT_TEXT_PLACEHOLDER ZBS(@"input_text_placeholder", @"Чё-нибудь...")
+#define kNZB_INPUT_BUTTON_TITLE ZBS(@"input_button_title", @"Написать")
+
+#define kNZB_MESSAGE_CELL_MY_MESSAGE_TEXT ZBS(@"message_cell_my_message_text", @"Моё сообщение")
